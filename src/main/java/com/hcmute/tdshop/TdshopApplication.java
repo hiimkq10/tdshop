a43a17aa2c24d2a259af417794318b197732a60f
Ke Quang <trankequang9221@gmail.com>
2022-11-17 19:29:50 +0700

    Init Project

---
+++ src/main/java/com/hcmute/tdshop/TdshopApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.tdshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcmute/tdshop/TdshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmute.tdshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
