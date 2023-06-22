a4f03894dff22b2de5e44b6000a1c862563da577
Ke Quang <trankequang9221@gmail.com>
2023-06-22 12:38:49 +0700

    Fix lalamove bugs

--- src/main/java/com/hcmute/tdshop/controller/TestController.java
+++ src/main/java/com/hcmute/tdshop/controller/TestController.java
@@ -113,7 +113,7 @@ public class TestController {
     logger.info(json);
     String signatureData = String.format("%d%nPOST%n/v3/quotations%n%n%s", time, json);
     logger.info(signatureData);
-    String signature = myEncode(apiSecret, "1685698441197%nPOST%n/v3/quotations\r\nHello world");
+    String signature = myEncode(apiSecret, "1685698441197%nPOST%n/v3/quotations\r\n\r\nHello world");
     return new DataResponse(signature);
   }
 
